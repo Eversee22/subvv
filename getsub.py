cedc8a72d6b3c089420f35e37a1fc07eb4148b14
xxx <xxx@example.com>
2020-01-27 11:31:44 +0800

    update getsub.py

--- getsub.py
+++ getsub.py
@@ -1,17 +1,42 @@
 import base64
 import os,sys
 import json
+import argparse
+import requests
 
+def arg_parser():
+    parser = argparse.ArgumentParser(description='get v2ray subscriptions')
+    parser.add_argument('-url',dest='url',help='the url of subscriptions')
+    parser.add_argument('-i',dest='ib64',default='inputb64',
+        help='the input file of base64 coding of subscriptions')
+    parser.add_argument('-o',dest='outf',default='TEMP',help='the output file of subscriptions')
+    
+    return parser.parse_args()
+    
+args=arg_parser()
+url=args.url
+ib64=args.ib64
+outf=args.outf
+
+if url is not None:
+    print(url)
+    try:
+        r=requests.get(url)
+        out=r.text
+    except Exception:
+        print('Url Error')
+else:
+    try:
+        with open(ib64) as f:
+            out=f.readline().strip()
+    except Exception as e:
+        print('File Error\n',e)
+        sys.exit(1)
+    
 pre='dm1lc3M6'
-try:
-	with open('inputb64') as f:
-		out=f.readline().strip()
-except Exception as e:
-	print('No file "inputb64"')
-	sys.exit(1)
 print('get b64',len(out))
 if out.find(pre)==-1:
-	print('Unsupport encoding')
+	print('Unsupport Encoding')
 	sys.exit(1)	
 vms=out.split(pre)
 vms=[v for v in vms if len(v)>0]
@@ -22,25 +47,23 @@ for v in vms:
 	except:
 		pass
 cnt=0
-f=None
-if len(sys.argv)>1:
-	f=open(sys.argv[1],'w')	
+f=open(outf,'w')
 for i,l in enumerate(allink):
-	try:
-		l=l[2:]
-		lks=l.split(b'vmess://')
-		for lk in lks:
-			conf=base64.b64decode(lk)
-			cnt+=1
-			# print('[%d]'%(cnt))
-			conf=json.loads(conf)
-			strr='[%d]\nhost: %s\nport: %s\nid: %s\nalterId: %s\nnet: %s\ntls: %s\npath: %s\nadd: %s\nps: %s\n'%(
-				cnt,conf['host'],conf['port'],conf['id'],conf['aid'],conf['net'],conf['tls'],conf['path'],conf['add'],conf['ps'])
-			print(strr)
-			if f is not None:
-				f.write(strr+'\n')
-	except:
-		pass
-if f is not None:
-	f.close()
+    try:
+        l=l[2:]
+        lks=l.split(b'vmess://')
+        for lk in lks:
+            conf=base64.b64decode(lk)
+            cnt+=1
+            try:
+                conf=json.loads(conf)
+                strr='[%d]\nserver: %s\nport: %s\nid: %s\nalterId: %s\nnet: %s\ntls: %s\npath: %s\nadd: %s\nps: %s\n'%(
+                    cnt,conf['add'],conf['port'],conf['id'],conf['aid'],conf['net'],conf['tls'],conf['path'],conf['add'],conf['ps'])
+            except Exception:
+                strr=conf.decode('utf-8')
+            print(strr)
+            f.write(strr+'\n')
+    except:
+        pass
+f.close()
 	
