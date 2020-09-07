b9e8ccad8551557c0a2761901af275c9ff4aa41b
Ghz <gyoten22@gmail.com>
2020-09-07 13:06:14 +0800

    add js gui

---
+++ js/server.js
@@ -0,0 +1,275 @@
+const http = require('http')
+const urllib = require('url')
+const request = require('request')
+const querystring = require('querystring')
+const fs = require('fs')
+// const {execFileSync} = require('child_process')
+
+var server = http.createServer()
+// const subscs = []
+const cachedUsers = []
+const dbusers = loadUsers()
+
+function loadUsers() {
+    let users = new Map()
+    if (fs.existsSync('db/users')){
+        if (fs.existsSync('db/users/db')){
+            let data = fs.readFileSync('db/users/db', 'utf-8')
+            let us = data.split('\n')
+            for (const e of us){
+                if (e.length === 0)
+                    continue
+                let [id, name, sid] = e.split(',')
+                let u = {'id': id, 'name': name, 'sid': sid}
+                if (fs.existsSync('db/users/'+id)){
+                    const userData = fs.readFileSync('db/users/'+id)
+                    u.index = cachedUsers.length
+                    cachedUsers.push(JSON.parse(userData))
+                }
+                users.set(id, u)
+            }
+        }
+    } else {
+        fs.mkdirSync('db/users',{recursive: true})
+    }
+    return users
+}
+
+function parseVm(data){
+    let buf = Buffer.from(data, 'base64')
+    return JSON.parse(buf.toString())
+}
+
+function parseTr(data) {
+    let t = urllib.parse('trojan://'+data)
+    let trobj = {}
+    trobj['id'] = t.auth
+    trobj['hostname'] = t.hostname
+    trobj['port'] = t.port
+    trobj['query'] = t.query
+    trobj['ps'] = decodeURI(t.hash.substring(1))
+    return trobj
+}
+
+function parseData(data) {
+    let buf = Buffer.from(data, 'base64')
+    let decstr = buf.toString()
+    let items = decstr.split('\n')
+    let outs = []
+    for (const e of items) {
+        if (e.length === 0)
+            continue
+        let ind = e.indexOf(':')
+        let schema = e.substring(0,ind).toLowerCase()
+        let rest = e.substring(ind + 3)
+        let result
+        if (schema === 'vmess'){
+            result = parseVm(rest)
+        } else if (schema === 'trojan') {
+            result = parseTr(rest)
+        } else{
+            result = rest
+        }
+        outs.push({type: schema, value: result})
+    }
+    return outs
+}
+
+function encodeVm(e) {
+    let stre = JSON.stringify(e.value)
+    let buf = Buffer.from(stre).toString('base64')
+    // buf = Buffer.from(e.type+'://'+buf).toString('base64')
+    return e.type+'://'+buf
+}
+
+function encodeTr(e) {
+    const stre = `${e.type}://${e.value.id}@${e.value.hostname}:${e.value.port}?${e.value.query}#${encodeURI(e.value.ps)}`
+    return stre
+}
+
+function download(url) {
+    return new Promise((resolve, reject) => {
+        request(url, function(err, response, body) {
+            if (!err && response.statusCode === 200){
+                // console.log(body)
+                resolve(body)
+            } else {
+                if (err)
+                    reject(err)
+                else
+                    reject(new Error('status: '+response.statusCode))
+            }
+        })
+    })
+}
+
+function dealPost (req) {
+    return new Promise((resolve, reject) => {
+        let t = ''
+        req.on('data', (chunk) => {
+            t += chunk
+        })
+        req.on('error', (err) => {
+            reject(err)
+        })
+        req.on('end', () => {
+            resolve(t)
+        })
+    })
+}
+
+function encode (items) {
+    let composels = []
+    for (const item of items) {
+        if (item.type === 'vmess')
+            composels.push(encodeVm(item))
+        else if (item.type === 'trojan')
+            composels.push(encodeTr(item))
+    }
+    return Buffer.from(composels.join('\n')).toString('base64')
+}
+
+function saveUserDB () {
+    let userS = []
+    for (const item of dbusers.entries()){
+        const val = item[1]
+        if (val !== null)
+            userS.push(`${val.id},${val.name},${val.sid}`)
+    }
+    fs.writeFile('db/users/db', userS.join('\n'), (err) => {
+        if (err) {throw err}
+    })
+}
+
+function saveAll() {
+    for (const u of cachedUsers) {
+        fs.writeFile(`db/users/${u.id}`, JSON.stringify(u), (err) => {
+            if (err) { throw err }
+        })
+    }
+    saveUserDB()
+    console.log('all saved')
+}
+
+function removeUser(id) {
+    const us = dbusers.get(id)
+    if (us != null) {
+        dbusers.set(id, null)
+        cachedUsers.splice(us.index, 1)
+        if (fs.existsSync('db/users/'+us.id)){
+            fs.unlink('db/users/'+us.id, (err) => {
+                if (err) { throw err}
+            })
+        }
+        if (fs.existsSync(`db/users/${us.sid}`)){
+            fs.unlink(`db/users/${us.sid}`, (err) => {
+                if (err) { throw err}
+            })
+        }
+        // saveUserDB()
+    }
+}
+
+server.on('request', async function (req, res) {
+    // console.log(req.url)
+    parts = urllib.parse(req.url, true)
+    // console.log(parts)
+    var resData = []
+    if (req.method === 'OPTIONS') {
+        res.setHeader('Access-Control-Allow-Origin', '*')
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST')
+        res.setHeader('Access-Control-Allow-Headers', req.headers["access-control-request-headers"])
+        res.writeHead(200, {'Content-Type': 'text/plain'})
+        res.end()
+        return
+    }
+    const pathname = parts.pathname
+    if (pathname === '/update'){
+        let url
+        if (req.method === 'GET') {
+            let params = parts.query
+            url = params.url
+        } else if (req.method === 'POST') {
+            const result = await dealPost(req)
+            let paramobj = JSON.parse(result)
+            // let paramobj = querystring.parse(t)
+            let params = []
+            for (const key in paramobj) {
+                if (key !== 'url')
+                    params.push(key + '=' + paramobj[key])
+            }
+            url = paramobj.url+'?'+params.join('&')
+        }  
+        if (url != null) {
+            let content = await download(url)
+            resData = parseData(content)
+        }
+    }
+    else if (pathname === '/uploadUser') {
+        if (req.method === 'POST') {
+            const data = await dealPost(req)
+            const user = JSON.parse(data)
+            let us = dbusers.get(user.id)
+            if (us == null) {
+                us = {'id': user.id, 'name': user.name, 'sid': user.subscId, 'index': cachedUsers.length}
+                dbusers.set(us.id, us)
+                // saveUserDB()
+            } else {
+                us.name = user.name
+                us.sid = user.subscId
+                // cachedUsers[us.index] = user
+            }
+            let encs = encode(user.selectedItems)            
+            fs.writeFile(`db/users/${us.sid}`, encs, (err) => {
+                if (err) {
+                    throw err
+                }
+                // console.log(us.name+'-'+us.sid+' saved')
+            })
+            user.encStr = encs
+            cachedUsers[us.index] = user
+            // fs.writeFileSync(`db/users/${us.id}`, JSON.stringify(user))
+            // console.log(us.name+'-'+us.id+' saved')
+            resData = encs
+        }
+    } else if (pathname === '/users') {
+        resData =  cachedUsers
+
+    } else if (pathname === '/removeUser') {
+        if (req.method === 'GET') {
+            removeUser(parts.query.id)
+            resData = 'ok'
+        }
+    } else if (pathname === '/saveAll') {
+        // console.log("saveAll")
+        saveAll()
+    } 
+    // else if (pathname === '/upload2Serv') {
+    //     let id = parts.query.id
+    //     let u = dbusers.get(id)
+    //     if (u != null) {
+    //         const output = execFileSync('./db/users/upload.bat',[`db/users/${u.name}-${u.sid}`])
+    //         console.log(output)
+    //     }
+    // }
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    // res.setHeader('Access-Control-Allow-Credentials', 'true')
+    res.writeHead(200, {'Content-Type': 'application/json; charset=utf-8'})
+    res.end(JSON.stringify(resData))
+})
+
+// server.on('close', () => {
+//     console.log('server closed')    
+// })
+
+// server.on('error', (err) => {
+//     console.log(err)
+// })
+
+server.listen(8000, () => {
+    console.log('server is listening on 8000...')
+})
+
+// process.on('beforeExit',(code) => {
+//     console.log('process exit, code: '+code)
+// })
---
+++ js/subs.js
@@ -0,0 +1,351 @@
+Vue.filter('date', time => moment(time).format('DD/MM/YY, HH:mm'))
+
+const baseUrl = "http://localhost:8000"
+const chars = '0123456789abcdefghijklmnopqrstuvwxyz'
+
+function generateId(len) {
+    let result = '';
+    let clen = chars.length
+    for (let i=len; i>0; --i) {
+        result += chars[Math.floor(Math.random()*clen)]
+    }
+    return result
+}
+
+function parseUrl(url) {
+    let ind = url.indexOf('?')
+    let rawurl = url.substring(0,ind)
+    let params = url.substring(ind+1)
+    let paramobj = {'url': rawurl}
+    params = params.split('&')
+    for (const param of params) {
+        let [k, v] = param.split('=')
+        paramobj[k] = v
+    }
+    return paramobj
+}
+
+// function postOptions(obj) {
+//     return {
+//         method: 'POST',
+//         mode: 'cors',
+//         headers: {
+//             'Content-Type': 'application/json'
+//         },
+//         body: JSON.stringify(obj)
+//     }
+// }
+
+async function fetchData (url, options) {
+    const response = await fetch(`${baseUrl}${url}`, options)
+    if (response.ok) {
+        const data = await response.json()
+        return data
+    } else {
+        const msg = await response.text()
+        console.log(msg)
+        return null
+    }
+}
+
+async function post (url, options) {
+    const finalOptions = Object.assign({}, {
+        method: 'POST',
+        mode: 'cors',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }, options)
+    const data = await fetchData(url, finalOptions)
+    return data
+}
+
+async function get(url, options) {
+    const finalOptions = Object.assign({}, {
+        method: 'GET', 
+        headers: {'Content-Type': 'text/plain'}
+    }, options)
+    const data = await fetchData(url, finalOptions)
+    return data
+}
+
+// window.onbeforeunload = function () {
+//     // console.log('leave')
+//     // if (confirm('Are you sure you want to leave this page?'))
+//     get('/saveAll')
+// }
+// window.onunload = function () {
+//     console.log('enter')
+// }
+
+// window.addEventListener('resize', () => {
+//     console.log(window.innerWidth, window.innerHeight)
+// })
+// window.addEventListener('onbeforeunload', () => {
+//     confirm('Are you sure you want to leave this page?')
+// })
+
+new Vue({
+    name: 'subvv',
+    el: '#subvv',
+
+    data() {
+        return {
+            subscs: JSON.parse(localStorage.getItem('subscs')) || [],
+            selectedId: localStorage.getItem('selected-id') || null,
+            selectedItems: [],
+            users: [],
+            selectedUserId: localStorage.getItem('selected-userid') || null
+            // info: ''
+        }
+    },
+
+    created() {
+        this.fetchUsers()
+        for (const subsc of this.subscs) {
+            if (subsc.content != null) {
+                for (const e of subsc.content) {
+                    if (e.selected)
+                        this.selectedItems.push(e)
+                }
+            }
+        }
+        
+    },
+
+    // mounted() {
+    //     // window.addEventListener('beforeunload', function () {
+    //     //     get('/saveAll')
+    //     // })
+    //     // console.log('mounted')
+    // },
+    // beforeDestroy() {
+    //     console.log('beforeDestroy')
+    //     get('/saveAll')
+    // },
+
+    // destroyed() {
+    //     console.log('destroyed')
+    //     // get('/saveAll')
+    //     // window.removeEventListener('beforeunload', e => this.beforeunloadFn(e))
+    // },
+
+    watch: {
+        subscs: {
+            handler: 'saveSubscs',
+            deep: true
+        },
+        selectedId(val) {
+            localStorage.setItem('selected-id', val)
+        },
+        selectedUserId(val) {
+            localStorage.setItem('selected-userid', val)
+        }
+        
+    },
+
+    computed: {
+        addButtonTitle () {
+            return this.subscs.length + ' subscription(s) already'
+        },
+        sortedSubscs() {
+            return this.subscs.slice().sort((a,b) => a.created - b.created)
+            .sort((a,b) => (a.marked == b.marked) ? 0 : a.marked ? -1 : 1)
+        },
+        selectedSubsc() {
+            return this.subscs.find(subsc => subsc.id == this.selectedId)
+        },
+        subscItemsCount() {
+            if (this.selectedSubsc){
+                if (this.selectedSubsc.content != null)
+                    return this.selectedSubsc.content.length
+                else 
+                    return 0
+            }   
+        },
+        addUserButtonTitle() {
+            return this.users.length+ ' user(s) already'
+        },
+        selectedUser() {
+            return this.users.find(user => user.id == this.selectedUserId)
+        },
+        selectedUserUuid() {
+            const items = this.selectedUser.selectedItems
+            if (items.length === 0)
+                return null
+            else 
+                return items[0].value.id
+        }  
+    },
+
+    methods: {
+        // beforeunloadFn(e) {
+        //     if (confirm('Are you sure you want to leave this page?'))
+        //         fetchData('/saveAll', {method: 'GET', headers: { 'Content-Type': 'text/plain'}})
+        // },
+
+        addSubsc() {
+            const time = Date.now()
+            const subsc = {
+                id: String(time),
+                title: 'New subscription '+ (this.subscs.length + 1),
+                url: '',
+                created: time,
+                updated: time,
+                marked: false,
+                content: null
+            }
+            this.subscs.push(subsc)
+        },
+        addUser() {
+            const time = Date.now()
+            const user = {
+                id: String(time),
+                name: 'New user '+(this.users.length + 1),
+                subscId: generateId(16),
+                created: time,
+                selectedItems: JSON.parse(JSON.stringify(this.selectedItems)),
+                encStr: ''
+            }
+            this.users.push(user)
+        },
+        selectUser(user) {
+            this.selectedUserId = user.id
+        },
+        selectSubsc(subsc) {
+            this.selectedId = subsc.id
+        },
+        saveSubscs() {
+            localStorage.setItem('subscs', JSON.stringify(this.subscs))
+        },
+        markSubsc() {
+            const subsc = this.selectedSubsc
+            subsc.marked ^= true
+            if (subsc.content == null)
+                return
+            for (const e of subsc.content){
+                if (subsc.marked) {
+                    if (!e.selected)
+                        this.selectedItems.push(e)
+                } else {
+                    if (e.selected)
+                        this.removeSubscItem(e)
+                }
+                e.selected = subsc.marked
+            }                
+        },
+        removeSubsc() {
+            if(this.selectedSubsc && confirm('Are you sure you want to delete this Subscription?')){
+                const subsc = this.selectedSubsc
+                const index = this.subscs.indexOf(subsc)
+                if (index !== -1) {
+                    this.subscs.splice(index, 1)
+                    if (subsc.content == null)
+                        return
+                    for (const e of subsc.content){
+                        e.selected = false
+                        this.removeSubscItem(e)
+                    }
+                }
+            }
+        },
+        async removeUser() {
+            if (this.selectedUser && confirm('Are you sure you want to delete this User?')) {
+                const user = this.selectedUser
+                const index = this.users.indexOf(user)
+                if (index != -1) {
+                    this.users.splice(index, 1)
+                    get(`/removeUser?id=${user.id}`)
+                }
+            }
+        },
+        removeSubscItem(item) {
+            const index = this.selectedItems.indexOf(item)
+            if (index !== -1) {
+                   this.selectedItems.splice(index, 1)
+            }
+        },
+        markSubscItem(item) {
+            item.selected ^= true
+            if (item.selected) {
+                this.selectedItems.push(item)
+            } else {
+                this.removeSubscItem(item)
+            }
+        },
+        markUserSubscItem(item) {
+            item.selected ^= true
+            const selectedItems = this.selectedUser.selectedItems
+            if (item.selected){
+                selectedItems.push(item)
+            } else {
+                const index = selectedItems.indexOf(item)
+                if (index !== -1)
+                    selectedItems.splice(index, 1)
+            }
+        },
+        restoreUserSubscs() {
+            this.selectedUser.selectedItems = JSON.parse(JSON.stringify(this.selectedItems))
+        },
+        async updateSubsc() {
+            const subsc = this.selectedSubsc
+            if (!subsc.url){
+                return
+            } 
+            const paramobj = parseUrl(subsc.url)
+            // console.log(paramobj)
+            // const options = postOptions(paramobj)
+            const data = await post('/update', {body: JSON.stringify(paramobj)})
+            if (data != null) {
+                for (const e of data){
+                    e.selected = subsc.marked
+                }
+                subsc.content = data
+                subsc.updated = Date.now()
+            }
+        },
+        // async encodeUserItems() {
+        //     const user = this.selectedUser
+        //     if (user.selectedItems.length === 0)
+        //         return
+        //     let obj = {
+        //         'name': user.name, 
+        //         'id': user.id, 
+        //         'sid': user.subscId,
+        //         'items': user.selectedItems
+        //     }
+        //     const options = postOptions(obj)
+        //     const data = await fetchData('/encode', options)
+        //     // console.log(data)
+        //     user.encStr = data
+        // },
+        async upload() {
+            const user = this.selectedUser
+            // const options = postOptions(user)
+            let data = await post('/uploadUser', {body: JSON.stringify(user)})
+            // console.log('upload status:', status)
+            user.encStr =  data
+        },
+        async fetchUsers() {
+            this.users = await get('/users', {})
+        },
+        saveAll() {
+            get('/saveAll')
+        },
+        async upload2Serv() {
+            // await get(`/upload2Serv?id=${this.selectedUser.id}`)
+        },
+        onUserIdChange(event) {
+            const id = event.currentTarget.value
+            // console.log('user id changed: '+id)
+            const items = this.selectedUser.selectedItems
+            for (const item of items) {
+                item.value.id = id
+            }
+        },
+        generateSid() {
+            this.selectedUser.subscId = generateId(16)
+        }
+    }
+    
+})
